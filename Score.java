fce3eb76ecdf03b5a71921fe6f1949de338f3d8b
Syed Ishaaq <syedishaaq48@gmail.com>
2024-07-21 18:20:41 +0530

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,8 @@
+public class Score {
+    public static void main(String[] args) {
+        String str = "hello";
+        char[] ch = str.toCharArray();
+       int n = ch[0]-ch[1];
+       System.out.println(n);
+    }
+}
