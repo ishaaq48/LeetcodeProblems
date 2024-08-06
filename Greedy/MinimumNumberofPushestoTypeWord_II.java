3894acf2a9c1b176f8fd7882dc1e8c1b2386b734
Syed Ishaaq <syedishaaq48@gmail.com>
2024-08-06 15:17:24 +0530

    Update MinimumNumberofPushestoTypeWord_II.java

--- Greedy/MinimumNumberofPushestoTypeWord_II.java
+++ Greedy/MinimumNumberofPushestoTypeWord_II.java
@@ -22,3 +22,27 @@ class Solution {
         return sum;
     }
 }
+// Approach 2
+// class Solution {
+//     public int minimumPushes(String word) {
+//         int[] arr = new int[26];
+//         int sum =0;
+//         for(int i =0; i < word.length();i++)
+//         {
+//             arr[word.charAt(i)- 'a']++; 
+//         }
+//         Integer[] sortedFreq = new Integer[26];
+//         for (int i = 0; i < 26; i++) {
+//             sortedFreq[i] = arr[i];
+//         }
+//         Arrays.sort(sortedFreq, Collections.reverseOrder());
+
+//         for(int j = 0; j < 26;j++)
+//         {
+//             if(sortedFreq[j] == 0) break;
+//             sum += (j / 8 + 1) * sortedFreq[j];
+
+//         }
+//         return sum;
+//     }
+// }
