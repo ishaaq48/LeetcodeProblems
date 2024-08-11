3966463f26067e8ba9d2d9e351a653474d7a6604
Syed Ishaaq <syedishaaq48@gmail.com>
2024-08-11 11:30:01 +0530

    Create Candy.java

---
+++ Greedy/Candy.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candi = new int[n];
+        int sum =0;
+        for(int i = 0; i < n; i++)
+        {
+            candi[i] = 1;
+        }
+        for(int i = 1; i < n; i++)
+        {
+            if(ratings[i] > ratings[i-1])
+            {
+                candi[i] = candi[i-1]+1;
+            }
+            
+        }
+        for(int i = n-2; i >=0; i--)
+        {
+            if(ratings[i]  > ratings[i+1])
+            {
+                candi[i] = Math.max(candi[i],candi[i+1]+1);
+            }
+        }
+        for(int i =0; i < n; i++)
+        {
+            sum += candi[i]; 
+        }
+        return sum;
+    }
+}
