af63b1a6dbfc90d36d60ebc8af6c040c5eb167da
Syed Ishaaq <syedishaaq48@gmail.com>
2024-08-16 18:34:48 +0530

    Update HappyNumber.java

--- Hash Tables/HappyNumber.java
+++ Hash Tables/HappyNumber.java
@@ -21,3 +21,31 @@ class Solution {
         return sum;
     }
 }
+class Solution {
+    private int getSquareSum(int n)
+    {
+        int currSum = 0;
+        while(n != 0)
+        {
+            int remainder = (n % 10);
+            currSum += remainder * remainder;
+            n /= 10;
+        }
+        return currSum;
+
+    }
+//optimal solution 
+//     public boolean isHappy(int n) {
+//         int slow = getSquareSum(n);
+//         int fast = getSquareSum(getSquareSum(n));
+
+//      //Fast and slow pointer approach
+//         while((slow != fast) && (fast != 1))
+//         {
+//             slow = getSquareSum(slow);
+//             fast = getSquareSum(getSquareSum(fast));
+//         }
+
+//         return fast == 1;
+//     }
+// }
