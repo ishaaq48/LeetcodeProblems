b48f0785707c645e9bc37b98d8936f6d5cc7972b
Syed Ishaaq <syedishaaq48@gmail.com>
2024-10-12 19:18:58 +0530

    Optimal Solution

--- Array/MergeInterval.java
+++ Array/MergeInterval.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.lang.*;
 public class MergeInterval {
     public static int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> {
@@ -11,21 +10,16 @@ public class MergeInterval {
         });
         List<int[] > ans = new ArrayList<>();
         for (int i = 0; i < intervals.length; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if (!ans.isEmpty() && end <= ans.get(ans.size()-1)[1]) {
-                continue;
+            
+            if(ans.isEmpty() || intervals[i][0] > ans.get(ans.size()-1)[1])
+            {
+                ans.add(intervals[i]);
             }
-            for (int j = i + 1; j < intervals.length; j++) {
-                if(intervals[j][0] <= end){
-                end = Math.max(end, intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else
+            {
+                ans.get(ans.size()-1)[1] = Math.max(ans.get(ans.size()-1)[1],intervals[i][1]);
+            
             }
-            ans.add(new int[] { start, end });
         }
 
         return ans.toArray(new int[ans.size()][2]);
