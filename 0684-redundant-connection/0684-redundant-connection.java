feb0e194e96574ee031513b534c18fbdef1d652d
Syed Ishaaq <syedishaaq48@gmail.com>
2025-01-29 13:22:10 +0530

    Time: 0 ms (100%), Space: 42.9 MB (97.08%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,37 +1,32 @@
 class Solution {
-
-    public boolean dfs(int node,int parent,boolean[] vis, List<List<Integer>> adj){
-        vis[node] = true;
-
-        for(Integer it: adj.get(node)){
-            if(vis[it] == false){
-                if(dfs(it,node,vis,adj)) return true;
-            }
-            else if(parent != it) return true;
+    public int find(int x,int[] parent){
+        if(parent[x] != x){
+            parent[x] = find(parent[x],parent);
         }
 
-        return false;
+        return parent[x];
     }
+    public boolean union(int u, int v,int[] parent){
+        int rootU = find(u,parent);
+        int rootV = find(v,parent);
 
+        if(rootU == rootV) return false;
+
+        parent[rootU] = rootV;
+        return true;
+    }
     public int[] findRedundantConnection(int[][] edges) {
-        List<List<Integer>> adj = new ArrayList<>();
         int n = edges.length;
-        int m = edges[0].length;
-        for(int i = 0; i <= n; i++){
-            adj.add(new ArrayList<>());
-        }
-        int V = adj.size();
-        for(int[] edge : edges){
-            int u = edge[0], v = edge[1];
+        int[] parent = new int[n + 1];
 
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-
-            boolean[] vis = new boolean[V];
-            if(dfs(u,-1,vis,adj) == true){
-                return edge;
-            }
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
         }
+
+        for(int[] edge: edges){
+            if(!union(edge[0], edge[1],parent)) return edge;
+        }
+
         return new int[0];
     }
 }
