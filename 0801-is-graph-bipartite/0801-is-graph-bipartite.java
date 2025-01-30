f6dafc06b902d7cf8d43a88072b411cacc2b6f5d
Syed Ishaaq <syedishaaq48@gmail.com>
2025-01-30 10:28:33 +0530

    Time: 0 ms (100%), Space: 44.8 MB (89.31%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,22 +1,15 @@
 class Solution {
 
-    public boolean bfs(int start, int V, int[] color,int[][] graph){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        color[start] = 0;
+    public boolean dfs(int node, int colour, int[] color, int[][] graph) {
+        color[node] = colour;
 
-        while(!q.isEmpty())
-        {
-            int node = q.poll();
-
-            for(int it: graph[node]){
-                if(color[it] == -1){
-                    color[it] = 1 - color[node];
-                    q.add(it);
-                }
-                else if(color[it] == color[node]){
+        for (int it : graph[node]) {
+            if (color[it] == -1) {
+                if (dfs(it, 1 - colour, color, graph) == false) {
                     return false;
                 }
+            } else if (color[it] == color[node]) {
+                return false;
             }
         }
 
@@ -27,13 +20,14 @@ class Solution {
         int V = graph.length;
         int[] color = new int[V];
 
-        for(int i = 0; i < V; i++){
+        for (int i = 0; i < V; i++) {
             color[i] = -1;
         }
 
-        for(int i = 0; i < V; i++){
-            if(color[i] == -1){
-                if(bfs(i,V,color,graph) == false) return false;
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                if (dfs(i, 0, color, graph) == false)
+                    return false;
             }
         }
 
