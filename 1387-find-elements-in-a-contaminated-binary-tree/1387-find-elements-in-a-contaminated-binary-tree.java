b21f0e176704ac5afd36356a17bb28f71def1602
Syed Ishaaq <syedishaaq48@gmail.com>
2025-02-21 15:01:38 +0530

    Time: 21 ms (72.24%), Space: 47.9 MB (27.76%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -1,53 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class FindElements {
     Set<Integer> set = new HashSet<>();
 
-    public void recover(TreeNode root, int x) {
-        if (root == null)
-            return;
-        Queue<TreeNode> q = new LinkedList<>();
-        root.val= 0;
-        q.add(root);
-        set.add(0);
-
-        while (!q.isEmpty()) {
-
-            TreeNode node = q.poll();
-
-            if (node.left != null) {
-                node.left.val = 2 * node.val + 1;
-                set.add(node.left.val);
-                q.add(node.left);
-            }
-
-            if (node.right != null) {
-                node.right.val = 2 * node.val + 2;
-                set.add(node.right.val);
-                q.add(node.right);
-            }
-        }
-
+    public void recover(TreeNode root,int x){
+        if(root == null) return;
+        root.val = x;
+        set.add(x);
+        recover(root.left, 2 * x + 1);
+        recover(root.right, 2 * x + 2);
     }
 
     public FindElements(TreeNode root) {
-        if (root != null)
-            recover(root, 0);
+        if(root != null) recover(root,0); 
     }
-
+    
     public boolean find(int target) {
         return set.contains(target);
     }
