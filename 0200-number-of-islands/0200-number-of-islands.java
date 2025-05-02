85fab8ee39858d9537674f805ed87cba9499b40f
Syed Ishaaq <syedishaaq48@gmail.com>
2025-05-02 19:13:43 +0530

    Time: 3 ms (84.26%), Space: 49.4 MB (71.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,54 +1,32 @@
-class Pair {
-    int first;
-    int second;
-
-    public Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
+    public static void dfs(int i,int j,int n,int m,char[][] grid) {
+        if(i < 0 || i >= n || j < 0 || j >= m) return;
+        if(grid[i][j] == '1') {
+            grid[i][j] = '#';
 
-    public void bfs(int r, int c, boolean[][] vis, char[][] grid) {
-        vis[r][c] = true;
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(r, c));
-        int n = grid.length;
-        int m = grid[0].length;
-        int[] delR = { -1, 0, 1, 0 };
-        int[] delC = { 0, 1, 0, -1 };
-        while (!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
-            q.remove();
-
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + delR[i];
-                int ncol = col + delC[i];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == false && grid[nrow][ncol] == '1') {
-                    vis[nrow][ncol] = true;
-                    q.add(new Pair(nrow, ncol));
-                }
-            }
+            dfs(i-1,j,n,m,grid);
+            dfs(i,j-1,n,m,grid);
+            dfs(i+1,j,n,m,grid);
+            dfs(i,j+1,n,m,grid);
         }
+        else return;
+
     }
-
     public int numIslands(char[][] grid) {
+        int ans = 0;
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
-        int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (vis[i][j] == false && grid[i][j] == '1') {
-                    cnt++;
-                    bfs(i, j, vis, grid);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == '1')
+                {
+                    dfs(i,j,n,m,grid);
+                    ans++;
                 }
             }
         }
 
-        return cnt;
+        return ans;
     }
 }
